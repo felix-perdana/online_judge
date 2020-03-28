4b25233e1d9d7d00b1636a4a1fbcb58ee398273b
Felix Perdana <felixperdana@Travelokadmins-MacBook-Pro.local>
2020-03-28 12:55:29 +0700

    add GCJ-19/qual/1-solution

--- GCJ-19/qual/1.py
+++ GCJ-19/qual/1.py
@@ -4,13 +4,11 @@ for t in range(1, t+1):
     num = input()
 
     ans1 = ""
-    ans2 = ""
     for c in num:
         if c != '4':
             ans1 += c
-            ans2 += '0'
         else:
             ans1 += '3'
-            ans2 += '1'
 
-    print("Case #{}: {} {}".format(t, ans1, ans2))
+
+    print("Case #{}: {} {}".format(t, ans1, int(num)-int(ans1)))
